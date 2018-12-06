190eba252cebe32ede3399ab7ca6aeecd36c6793
susanvijoymathew <45573034+susanvijoymathew@users.noreply.github.com>
2018-12-06 16:43:59 -0600

    Update RestaurantPromotionController.java
    Date fields are strings in forms. They are converted to Java Date objects in this call. Added Date validation.

--- src/main/java/com/mcd/mars/controller/RestaurantPromotionController.java
+++ src/main/java/com/mcd/mars/controller/RestaurantPromotionController.java
@@ -1,21 +1,27 @@
 package com.mcd.mars.controller;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mcd.mars.business.FilterForm;
+import com.mcd.mars.business.PromotionForm;
 import com.mcd.mars.business.RestaurantPromotion;
 import com.mcd.mars.business.service.RestaurantPromotionService;
+import com.mcd.mars.utility.MarsDataUtility;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/mars")
 public class RestaurantPromotionController {
 	private RestaurantPromotionService promotionService;
 	
@@ -31,21 +37,89 @@ public class RestaurantPromotionController {
 			model.addAttribute("locations", promotionService.getAllAreas());
 		}
 		
+		if (!model.containsAttribute("promotionForm")) {
+			model.addAttribute("promotionForm", new PromotionForm());
+			model.addAttribute("products", promotionService.getAllProducts());
+		}
+		
 		if (!model.containsAttribute("restaurantPromotionList"))
 			model.addAttribute( promotionService.getAllPromotions() );
 		
+		/*
+		List<com.mcd.mars.data.entity.Area> allAreas = promotionService.getAllAreas();
+		System.out.println("Printing the areas retrieved from Database");
+		String space = "    ";
+		for (com.mcd.mars.data.entity.Area a: allAreas) {
+			String spaces = (new String(new char[a.getLevel()]).replace("\0", space));
+			System.out.println(spaces + a);
+		}
+		*/
 		return "home";
 	}
 	
 	@RequestMapping(value="/filter", method=RequestMethod.POST)
-	public String filter(FilterForm filterForm, RedirectAttributes model) {
+	public String filter(@Valid FilterForm filterForm, BindingResult result, RedirectAttributes model) {
+		Date sDate = null;
+		try {
+			sDate = MarsDataUtility.getAsDate(filterForm.getStartDate());
+		} catch(ParseException pe) {
+			result.rejectValue("endDate", "Invalid date. Please enter in MM/DD/YYYY format (example, 01/20/2018).");
+		}
+
+		Date eDate = null;
+		try {
+			eDate = MarsDataUtility.getAsDate(filterForm.getEndDate());
+		} catch(ParseException pe) {
+			result.rejectValue("endDate", "Invalid date. Please enter in MM/DD/YYYY format (example, 12/03/2018).");
+		}
+		
+		if (eDate.before(sDate)) {
+			result.rejectValue("endDate", "Promotion end date must come after the promotion start date.");
+		}
+		
+		if (result.hasErrors()) {
+			model.addAttribute("FilterForm", filterForm);
+			return "home";
+		}
+		
 		List<RestaurantPromotion> promotions = 
-			promotionService.getPromotionsByFilterSelections(
-				filterForm.getStartDate(), filterForm.getEndDate(), filterForm.getLocation());
+			promotionService.getPromotionsByFilterSelections(sDate, eDate, filterForm.getLocation());
 		
 		model.addAttribute("filterForm", filterForm);
 		model.addAttribute(promotions);
 		
-		return "redirect:/";
+		return "redirect:/mars";
+	}
+	
+	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public String create(@Valid PromotionForm promoForm, BindingResult result, Model model) {
+		Date sDate = null;
+		try {
+			sDate = MarsDataUtility.getAsDate(promoForm.getStartDate());
+		} catch(ParseException pe) {
+			result.rejectValue("endDate", "Invalid date. Please enter in MM/DD/YYYY format (example, 01/20/2018).");
+		}
+
+		Date eDate = null;
+		try {
+			eDate = MarsDataUtility.getAsDate(promoForm.getEndDate());
+		} catch(ParseException pe) {
+			result.rejectValue("endDate", "Invalid date. Please enter in MM/DD/YYYY format (example, 12/03/2018).");
+		}
+		
+		if (eDate.before(sDate)) {
+			result.rejectValue("endDate", "Promotion end date must come after the promotion start date.");;
+		}
+		
+		if (result.hasErrors()) {
+			model.addAttribute("PromotionForm", promoForm);
+			return "home";
+		}
+		
+		promotionService.addPromotion(
+			promoForm.getName(), promoForm.getDescription(),
+			sDate, eDate,
+			promoForm.getArea(), promoForm.getProduct());
+		return "redirect:/mars";
 	}
 }
