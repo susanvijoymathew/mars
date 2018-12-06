00ccdd6164683b52198a179ad63f4678ac9022c8
susanvijoymathew <45573034+susanvijoymathew@users.noreply.github.com>
2018-12-06 17:02:10 -0600

    Update FilterForm.java

--- src/main/java/com/mcd/mars/business/FilterForm.java
+++ src/main/java/com/mcd/mars/business/FilterForm.java
@@ -3,9 +3,9 @@ package com.mcd.mars.business;
 import javax.validation.constraints.NotNull;
 
 public class FilterForm {
-	@NotNull(message="Please enter a date in MM/dd/YYYY format.")
+	@NotNull(message="Please enter a date in MM-dd-YYYY format.")
 	private String startDate;
-	@NotNull(message="Please enter a date in MM/dd/YYYY format.")
+	@NotNull(message="Please enter a date in MM-dd-YYYY format.")
 	private String endDate;
 	long location;
 	
