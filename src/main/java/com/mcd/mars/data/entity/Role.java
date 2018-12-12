46543d26db177313d5fa85c054420b0ea9702adb
susanvijoymathew <45573034+susanvijoymathew@users.noreply.github.com>
2018-12-12 16:08:50 -0600

    Update Role.java

--- src/main/java/com/mcd/mars/data/entity/Role.java
+++ src/main/java/com/mcd/mars/data/entity/Role.java
@@ -34,6 +34,31 @@ public class Role {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Role - [id: " + id + ", role: " + role + "]";
 	}
