4139a7fb282cb8e07014075e56ecdc49a79cc9db
susanvijoymathew <45573034+susanvijoymathew@users.noreply.github.com>
2018-12-12 16:12:11 -0600

    Update PromotionRepository.java

--- src/main/java/com/mcd/mars/data/repository/PromotionRepository.java
+++ src/main/java/com/mcd/mars/data/repository/PromotionRepository.java
@@ -13,10 +13,10 @@ public interface PromotionRepository extends CrudRepository<Promotion, Long> {
 	public List<Promotion> findAll();
 	public Promotion findById(long id);
 	
-	@Query(value="select * from promotion where date(start_date) <= date(NOW()) or date(end_date) >= date(NOW())", nativeQuery=true)
+	@Query(value="select * from promotion where start_date <= NOW() or end_date >= NOW()", nativeQuery=true)
 	public List<Promotion> findAllActivePromotions();
 	
-	@Query(value="select * from promotion where date(start_date) >= ?1) and date(end_date) <= ?2)", nativeQuery=true)
+	@Query(value="select * from promotion where start_date >= ?1 and end_date <= ?2", nativeQuery=true)
 	public List<Promotion> findByStartAndEndDates(Date startDate, Date endDate);
 	
 	public List<Promotion> findByStartDateGreaterThanEqual(Date startDate);
