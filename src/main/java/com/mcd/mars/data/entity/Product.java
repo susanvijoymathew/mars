e87e9fe51f5145456d8f69310c985c169dbca2ef
susanvijoymathew <45573034+susanvijoymathew@users.noreply.github.com>
2018-12-12 16:06:48 -0600

    Update Product.java

--- src/main/java/com/mcd/mars/data/entity/Product.java
+++ src/main/java/com/mcd/mars/data/entity/Product.java
@@ -44,6 +44,37 @@ public class Product {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Product - [id: " + id + ", name: " + name + ", category: " + category + "]";
 	}
