50d8135c406b1539212c89b3946961ae29da46f3
susanvijoymathew <45573034+susanvijoymathew@users.noreply.github.com>
2018-12-12 16:09:13 -0600

    Update User.java

--- src/main/java/com/mcd/mars/data/entity/User.java
+++ src/main/java/com/mcd/mars/data/entity/User.java
@@ -77,6 +77,52 @@ public class User {
     }
     
     @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((active == null) ? 0 : active.hashCode());
+		result = prime * result + ((emailAddress == null) ? 0 : emailAddress.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + (int) (roleId ^ (roleId >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (active == null) {
+			if (other.active != null)
+				return false;
+		} else if (!active.equals(other.active))
+			return false;
+		if (emailAddress == null) {
+			if (other.emailAddress != null)
+				return false;
+		} else if (!emailAddress.equals(other.emailAddress))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (roleId != other.roleId)
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "User - [id: " + id + ", firstName: " + firstName + ", lastName: " + lastName + ", emailAddress: " + emailAddress + ", roleId: " + roleId + ", active: " + active + " ]";
 	}
